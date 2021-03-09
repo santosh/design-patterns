7fd3129ff106bfc697ab9b3e313139b03f8dc918
Santosh Kumar <sntshkmr60@gmail.com>
2021-03-09 21:10:54 +0000

    Adds Telescoping constructor

---
+++ creational/javascript/builder/index.js
@@ -0,0 +1,10 @@
+const Person = require('./Person')
+
+// Employees 
+let sue = new Person('Sue', true, true, 60)
+let bill = new Person('Bill', true, false, 20)
+let phil = new Person('Phil', true, false)
+
+// Shoppers
+let charles = new Person('Charles', false, false, 0, 500, ['jeans', 'sunglasses'])
+let tabbitha = new Person('Tabbitha', false, false, 0, 100)
