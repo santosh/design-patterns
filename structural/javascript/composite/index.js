cfcb424aa732997d352f5932c8d5911ea30d3858
Santosh Kumar <sntshkmr60@gmail.com>
2021-04-04 17:47:00 +0000

    Adds group element with same interface to individual elem
    Adds composite to calculate total and print element.

--- structural/javascript/composite/index.js
+++ structural/javascript/composite/index.js
@@ -1,16 +1,16 @@
 const CatalogItem = require('./CatalogItem')
-// const CatalogGroup = require('./CatalogGroup')
+const CatalogGroup = require('./CatalogGroup')
 
 let boots = new CatalogItem("Leather Boots", 79.99)
 let sneakers = new CatalogItem("Kicks", 39.99)
 let flipFlops = new CatalogItem("California work boots", 19.99)
 
-// let group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops])
+let group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops])
 
 console.log('boots total: ', `$${boots.total}`);
-// console.log('shoes total: ', `$${group_shoes.total}`)
+console.log('shoes total: ', `$${group_shoes.total}`)
 
 boots.print()
 sneakers.print()
 
-// group_shoes.print()
+group_shoes.print()
