6897dd2d8ce30b363126b6a3ab67986ac791017d
Santosh Kumar <sntshkmr60@gmail.com>
2021-04-07 15:48:40 +0000

    Adds mockup for Strategy pattern

--- behavorial/javascript/strategy/index.js
+++ behavorial/javascript/strategy/index.js
@@ -4,3 +4,9 @@ logger.log('Hello World')
 logger.log('Hi World')
 logger.log('Yo World')
 
+logger.changeStrategy('toFile')
+
+logger.log('Hello World')
+logger.log('Hi World')
+logger.log('Yo World')
+
