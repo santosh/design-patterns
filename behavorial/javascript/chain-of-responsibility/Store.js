fd9c87bf50968b8b532e948c8fd30293a1d994ad
Santosh Kumar <sntshkmr60@gmail.com>
2021-04-07 10:38:17 +0000

    Adds mockup and system requirement for COR

--- behavorial/javascript/chain-of-responsibility/Store.js
+++ behavorial/javascript/chain-of-responsibility/Store.js
@@ -1,12 +1,23 @@
+var Storage = require('./Storage');
+
 class Store {
   constructor(name, inventory = []) {
     this.name = name;
-    this.inventory = inventory
+
+    let floor = new Storage('store floor', inventory.floor)
+    let backroom = new Storage('store backroom', inventory.backroom)
+    let localStore = new Storage('nearby store', inventory.localStore, 1)
+    let warehouse = new Storage('warehouse', inventory.warehouse, 5)
+
+    floor.setNext(backroom)
+    backroom.setNext(localStore)
+    localStore.setNext(warehouse)
+
+    this.store = floor
   }
 
   find(itemName) {
-    let index = this.inventory.map(item => item.name).indexOf(itemName);
-    return this.inventory[index]
+    return this.storage.find(itemName)
   }
 }
 
