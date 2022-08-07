e9d4861f6be0130c95affb804529d08f35b8d5de
Santosh Kumar <sntshkmr60@gmail.com>
2022-08-07 09:34:54 +0530

    move definition to single file

--- creational/golang/abstract-factory/adidasShirt.go
+++
@@ -1,6 +0,0 @@
-package main 
-
-type AdidasShirt struct {
-	Shirt
-}
-
---
+++ creational/golang/abstract_factory/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Shirt struct {
+	logo string
+	size int
+}
+
+func (s *Shirt) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *Shirt) getLogo() string {
+	return s.logo
+}
+
+func (s *Shirt) setSize(size int) {
+	s.size = size
+}
+
+func (s *Shirt) getSize() int {
+	return s.size
+}
+
+type Shoe struct {
+	logo string
+	size int
+}
+
+func (s *Shoe) setLogo(logo string) {
+	s.logo = logo
+}
+
+func (s *Shoe) getLogo() string {
+	return s.logo
+}
+
+func (s *Shoe) setSize(size int) {
+	s.size = size
+}
+
+func (s *Shoe) getSize() int {
+	return s.size
+}
+
+type IShirt interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type IShoe interface {
+	setLogo(logo string)
+	setSize(size int)
+	getLogo() string
+	getSize() int
+}
+
+type Adidas struct {
+}
+
+type AdidasShirt struct {
+	Shirt
+}
+
+type AdidasShoe struct {
+	Shoe
+}
+
+func (a *Adidas) makeShoe() IShoe {
+	return &AdidasShoe{
+		Shoe: Shoe{
+			logo: "adidas",
+			size: 8,
+		},
+	}
+}
+
+func (a *Adidas) makeShirt() IShirt {
+	return &AdidasShirt{
+		Shirt: Shirt{
+			logo: "adidas",
+			size: 40,
+		},
+	}
+}
+
+type ISportsFactory interface {
+	makeShoe() IShoe
+	makeShirt() IShirt
+}
+
+func GetSportsFactory(brand string) (ISportsFactory, error) {
+	if brand == "adidas" {
+		return &Adidas{}, nil
+	}
+
+	if brand == "nike" {
+		return &Nike{}, nil
+	}
+
+	return nil, fmt.Errorf("wrong brand type passed")
+}
+
+type Nike struct {
+}
+
+func (n *Nike) makeShoe() IShoe {
+	return &NikeShoe{
+		Shoe: Shoe{
+			logo: "nike",
+			size: 8,
+		},
+	}
+}
+
+func (n *Nike) makeShirt() IShirt {
+	return &NikeShirt{
+		Shirt: Shirt{
+			logo: "nike",
+			size: 40,
+		},
+	}
+}
+
+type NikeShirt struct {
+	Shirt
+}
+
+type NikeShoe struct {
+	Shoe
+}
+
+func main() {
+	adidasFactory, _ := GetSportsFactory("adidas")
+	nikeFactory, _ := GetSportsFactory("nike")
+
+	nikeShoe := nikeFactory.makeShoe()
+	nikeShirt := nikeFactory.makeShirt()
+
+	adidasShoe := adidasFactory.makeShoe()
+	adidasShirt := adidasFactory.makeShirt()
+
+	printShoeDetails(nikeShoe)
+	printShirtDetails(nikeShirt)
+
+	printShoeDetails(adidasShoe)
+	printShirtDetails(adidasShirt)
+}
+
+func printShoeDetails(s IShoe) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
+
+func printShirtDetails(s IShirt) {
+	fmt.Printf("Logo: %s", s.getLogo())
+	fmt.Println()
+	fmt.Printf("Size: %d", s.getSize())
+	fmt.Println()
+}
